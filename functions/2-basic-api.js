f7c752a15257f89fa226a2dafaa5104a3961a54d
EasyCode <ezekielayodele1@gmail.com>
2024-04-10 17:52:45 +0100

    cors error fixed

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -2,6 +2,9 @@ const items = require('../assets/data')
 
 exports.handler = async () => {
   return {
+    headers: {
+      'Acess-Control-Allow-Origin': '*',
+    },
     statusCode: 200,
     body: JSON.stringify(items),
   }
