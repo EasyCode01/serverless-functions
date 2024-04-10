5462e05cc225a1ee3b6abd699c6cb152c505473a
EasyCode <ezekielayodele1@gmail.com>
2024-04-10 12:35:36 +0100

    Create a netlify.toml file and make a get request from the frontend

--- functions/1-hello.js
+++ functions/1-hello.js
@@ -1,6 +1,7 @@
+// domain/.netlify/functions/1-hello
 exports.handler = async () => {
   return {
-    statusCode: 200,
+    statusCode: 201,
     body: 'Hello, This is my first netlify serveless function',
   }
 }
